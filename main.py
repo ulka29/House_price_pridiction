00b9d1e7b1daf8b0930e4b4875d8e31308b796c1
ulka29 <patelulka0229@gmail.com>
2024-05-30 12:34:02 +0530

    initial

---
+++ main.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from flask import Flask,render_template, request
+import pickle
+
+
+data = pd.read_csv("cleaned_data.csv")
+app = Flask(__name__)
+pipe= pickle.load(open('RidgeModel1.pkl', 'rb'))
+
+
+@app.route('/')
+def index():
+    locations = sorted(data['location'].unique())
+    return render_template('index.html', locations = locations)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    location=request.form.get('location')
+    bhk=float(request.form.get('bhk'))
+    bath=float(request.form.get('bathrooms'))
+    sqft=float(request.form.get('sqft'))
+
+    input = pd.DataFrame([[location,sqft,bath,bhk]],columns=['location','total_sqft', 'bath', 'bhk'])
+    print(input.info())
+
+    pridiction = pipe.predict(input)[0]
+    return str(pridiction)
+
+if __name__=="__main__":
+    app.run()
